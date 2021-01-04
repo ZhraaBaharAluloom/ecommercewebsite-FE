3974985ab91982dee30b0bf13e2bfc0bb1edc0dc
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-01-04 14:10:08 -0800

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { observer } from "mobx-react";
+
+// Styles
+import { GlobalStyle, ThemeButton, InstagramIconStyle } from "./Styles";
+import { ThemeProvider } from "styled-components";
+import { FaInstagramSquare } from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io";
+
+// Components
+import NavBar from "./components/NavBar/NavBar";
+import Routes from "./components/routes/Routes";
+import Loading from "./components/loading/Loading";
+
+// Stores
+import shopStore from "./stores/shopStore";
+import pastaStore from "./stores/pastaStore";
+
+const theme = {
+  light: {
+    mainColor: "#242423",
+    backgroundColor: "#FEF9EC",
+  },
+  dark: {
+    mainColor: "#E9C46A",
+    backgroundColor: "#242423",
+  },
+  pink: {
+    mainColor: "white",
+    backgroundColor: "black",
+  },
+};
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const handleToggle = () => {
+    if (currentTheme === "light") {
+      setCurrentTheme("dark");
+    } else if (currentTheme === "dark") {
+      setCurrentTheme("pink");
+    } else {
+      setCurrentTheme("light");
+    }
+    // setCurrentTheme(currentTheme === "light" ? "dark" : "light");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <ThemeProvider theme={theme[currentTheme]}>
+      <GlobalStyle />
+      <NavBar handleToggle={handleToggle} currentTheme={currentTheme} />
+      {/* <ThemeButton onClick={handleToggle}>
+        {currentTheme === "light" ? "Dark Mode" : "Light Mode"}
+      </ThemeButton> */}
+      {shopStore.loading || pastaStore.loading ? <Loading /> : <Routes />}
+      <InstagramIconStyle>
+        <a href="https://instagram.com/pasta.and.basta?igshid=pf8sfbp9qyd8">
+          <FaInstagramSquare color="red" size="20" />
         </a>
-      </header>
-    </div>
+        <a href="">
+          <IoLogoFacebook size="24" />
+        </a>
+      </InstagramIconStyle>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default observer(App);
