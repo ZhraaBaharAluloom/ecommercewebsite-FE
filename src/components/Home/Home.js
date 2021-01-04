902bb73b69e0ac1228b42718a7729902ddd67621
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-01-04 14:15:14 -0800

    Home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Styles
+import { TextStyle } from "./Styles";
+
+const Home = () => {
+  return (
+    <TextStyle>
+      <img src="https://assets.stickpng.com/thumbs/589c828564b351149f22a82a.png" />
+      Welcome That Pasta Home
+    </TextStyle>
+  );
+};
+
+export default Home;
---
+++ src/components/Home/Styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const TextStyle = styled.p`
+  text-align: center;
+  font-size: 25px;
+  font-weight: bold;
+  margin-top: 50px;
+
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  img {
+    padding-right: 2px;
+    width: 15em;
+  }
+`;
