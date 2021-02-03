1a27f1420a56a89c1a6eef5f74f92f53c10cbd13
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-02-03 06:40:25 -0800

    fixed home style

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,17 @@ import React from "react";
 import HomeImage from "./HomeImage.png";
 
 // Styles
-import { TextStyle } from "./Styles";
+import { HomeStyle, TextStyle } from "./Styles";
 
 const Home = () => {
   return (
-    <TextStyle>
+    <HomeStyle className="row">
       <img src={HomeImage} alt="pasta" />
-      Welcome to That Pasta
-    </TextStyle>
+      <TextStyle className="col-sm-12 col-md-6">
+        Welcome to That Pasta
+        <p>Where All Good Restaurants Gather!</p>
+      </TextStyle>
+    </HomeStyle>
   );
 };
 
--- src/components/Home/Styles.js
+++ src/components/Home/Styles.js
@@ -1,14 +1,21 @@
 import styled from "styled-components";
 
-export const TextStyle = styled.p`
-  text-align: center;
-  font-size: 25px;
-  font-weight: bold;
-  margin-top: 50px;
+export const HomeStyle = styled.p`
+  margin: 10em 2em auto 2em;
+  display: flex;
+  justify-content: center;
 
-  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
   img {
     padding-right: 2px;
     width: 15em;
+    display: inline-flex;
   }
+`;
+
+export const TextStyle = styled.div`
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 0.5em;
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 `;
