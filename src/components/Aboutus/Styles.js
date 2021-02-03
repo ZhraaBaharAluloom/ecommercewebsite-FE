15c96a9dda9dbcf6a9d980e6c1488567c26cfb27
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-02-03 06:38:43 -0800

    about us page

---
+++ src/components/Aboutus/Styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const TextWrapper = styled.div`
+  margin: 5em 2em auto 2em;
+  justify-content: center;
+  border: 10px solid grey;
+  padding: 2em;
+  border-style: double;
+  p {
+    text-align: justify;
+    text-align-last: center;
+    font-size: 20px;
+    font-weight: bold;
+    font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  }
+`;
